85c9caead7f39bbcc88e490f6e77d3c6cfbe6ec0
Nagabtac <darwin.catbagan@lorma.edu>
2025-11-14 13:26:24 +0800

    menu option. new branch

--- mysql.java
+++ mysql.java
@@ -3,7 +3,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.util.Scanner; // Import the Scanner class
+import java.util.Scanner; 
 
 public class mysql {
 
